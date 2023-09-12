0d6a6286d73d3a075e59ed05a1ec3d99a2dcb68c
Kody M <kodylmontes@gmail.com>
2023-09-11 18:45:32 -0700

    changed to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ interface Props {
   name: string;
 }
 
-class App extends React.Component<Props> {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>Hello {name}</h1>
-      </>
-    );
-  }
-}
+const App: React.FC<Props> = ({ name }) => {
+  return (
+    <>
+      <h1>Hello {name}</h1>
+    </>
+  );
+};
 
 export default App;
